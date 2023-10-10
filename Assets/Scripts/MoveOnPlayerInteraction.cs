3cdc0235447d18455430fe211a36a6e13fc6a320
Brandon McDonald <bamcd0313@gmail.com>
2023-10-10 19:25:12 -0400

    Changed bird script to work for any object

---
+++ Assets/Scripts/MoveOnPlayerInteraction.cs
@@ -0,0 +1,75 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using GD.MinMaxSlider;
+
+public class MoveOnPlayerInteraction : MonoBehaviour
+{
+
+    Animator _animator;
+
+    [TooltipAttribute("The object to move to when spooked.")]
+    [SerializeField] GameObject _targetObject;
+    [SerializeField] float _speed = 3f;
+    
+    [SerializeField] string _animationName;
+
+    bool _isUsingAnimator = false;
+    [MinMaxSlider(-10f, 10f)]
+    [SerializeField] Vector2 _randomXRange = new Vector2(2f, 8f);
+    [MinMaxSlider(-10f, 10f)]
+    [SerializeField] Vector2 _randomYRange = new Vector2(2f, 8f);
+    // Start is called before the first frame update
+    void Start()
+    {
+        if (GetComponent<Animator>() != null)
+        { _animator = GetComponent<Animator>();
+            _isUsingAnimator = true;
+        }
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    public void OnPlayerInteraction()
+    {
+        ;
+        if (_isUsingAnimator)
+        {
+            _animator.SetBool(_animationName, true);
+            print(_animationName);
+        }
+        StartCoroutine(MoveToTarget());
+    }
+
+
+
+    IEnumerator MoveToTarget()
+    {
+        Vector3 targetPosition = _targetObject.transform.position;
+        
+            targetPosition.x += Random.Range(_randomXRange.x, _randomXRange.y);
+        
+            targetPosition.y += Random.Range(_randomYRange.x, _randomYRange.y);
+        
+        while (transform.position != targetPosition)
+        {
+            transform.position = Vector3.MoveTowards(
+            transform.position, targetPosition,
+                _speed * Time.deltaTime);
+            yield return new WaitForSeconds(Time.deltaTime);
+        }
+
+        yield return new WaitForSeconds(1.5f);
+        if (_isUsingAnimator)
+        {
+            _animator.SetBool(_animationName, false);
+        }
+    }
+}
+
+
---
+++ Assets/Scripts/Objects/PlayerBag.cs
@@ -0,0 +1,53 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerBag : MonoBehaviour, IInteractable
+{
+    [SerializeField] Color _highlightColor;
+    [SerializeField] GameObject[] _birds;
+    bool _hasScaredBirds;
+    Color _defaultColor;
+    Vector3 _defaultScale;
+
+
+
+    void Start()
+    {
+        _defaultColor = GetComponent<SpriteRenderer>().color;
+        _defaultScale = transform.localScale;
+        
+    }
+
+    public void OnPlayerApproach()
+    {
+        //TODO: change to a sprite swap
+        GetComponent<SpriteRenderer>().color = _highlightColor;
+        //make object slightly bigger
+        transform.localScale = _defaultScale * 1.1f;
+    }
+
+    public void OnPlayerInteract()
+    {
+        //TODO: make this do something meaningful
+        Debug.Log("Player interacted with " + gameObject.name);
+        //scare all of the birds
+        if (_hasScaredBirds)
+        {
+            return;
+        }
+        foreach (GameObject bird in _birds)
+        {
+            bird.GetComponent<MoveOnPlayerInteraction>().OnPlayerInteraction();
+        }
+
+        _hasScaredBirds = true;
+
+    }
+
+    public void ResetToDefaults()
+    {
+        GetComponent<SpriteRenderer>().color = _defaultColor;
+        transform.localScale = _defaultScale;
+    }
+}
