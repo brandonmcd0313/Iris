92f13a32d3a9a6e78845f0da0529a0711bd146e6
Brandon McDonald <bamcd0313@gmail.com>
2023-10-17 09:07:35 -0400

    poster work

---
+++ Assets/Scripts/Objects/MissingPoster.cs
@@ -0,0 +1,42 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MissingPoster : MonoBehaviour, IInteractable
+{
+    [SerializeField] Color _defaultColor;
+    [SerializeField] Color _highlightColor;
+    [SerializeField] GameObject _posterPrefab;
+    SpriteRenderer _sr;
+    
+    void Start()
+    {
+        //set the default color
+        _sr = GetComponent<SpriteRenderer>();
+        _sr.color = _defaultColor;
+    }
+    
+    public void OnPlayerApproach()
+    {
+        _sr.color = _highlightColor;
+    }
+
+    public void OnPlayerInteract()
+    {
+        //pop up the poster
+        print("it is implemented shut up");
+    }
+
+    public void ResetToDefaults()
+    {
+        _sr.color = _defaultColor;
+    }
+    
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    
+}
