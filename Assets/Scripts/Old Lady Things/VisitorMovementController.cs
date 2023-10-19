c0851976e755b3287745618fd78ddca9a591f97e
Brandon McDonald <bamcd0313@gmail.com>
2023-10-19 14:03:26 -0400

    OLD LADY WORKS

--- Assets/Scripts/Old Lady Things/VisitorMovementController.cs
+++ Assets/Scripts/Old Lady Things/VisitorMovementController.cs
@@ -8,6 +8,7 @@ public class VisitorMovementController : MonoBehaviour
     [SerializeField] float _interactionDelay;
     [SerializeField] GameObject _oldLady;
     [SerializeField] GameObject _speechBubblePrefab;
+    [SerializeField] Vector3 _speechBubblePosition;
     [SerializeField] GameObject[] _visitorPrefabs;
     [SerializeField] Vector3 _visitorSpawnPoint;
     [SerializeField] Vector3 _visitorDestination;
@@ -16,9 +17,11 @@ public class VisitorMovementController : MonoBehaviour
     //must be awake to happen before the scene manager is created
     void Awake()
     {
-        if(!PlayerPrefsManager.HasPlayerPrefBeenActivated(_sceneLoadEvent))
+        _oldLady.GetComponent<Animator>().enabled = false;
+        if (!PlayerPrefsManager.HasPlayerPrefBeenActivated(_sceneLoadEvent))
         {
-            
+            StartCoroutine(VisitorLoop());
+         
         }
         else
         {
@@ -42,18 +45,31 @@ public class VisitorMovementController : MonoBehaviour
         {
             GameObject visitor = Instantiate(_visitorPrefabs[i], _visitorSpawnPoint, Quaternion.identity);
             StartCoroutine(MoveToPositon(visitor));
+
+
             //wait for half the interaction time
             yield return new WaitForSeconds(_interactionTime / 2);
+
             //spawn a speech bubble
-            GameObject speechBubble = Instantiate(_speechBubblePrefab, visitor.transform.position, Quaternion.identity);
+            GameObject speechBubble = Instantiate(_speechBubblePrefab, _speechBubblePosition, Quaternion.identity);
+            _oldLady.GetComponent<Animator>().enabled = true;
+
             //wait for the other half of the interaction time
             yield return new WaitForSeconds(_interactionTime / 2);
+
             //destroy the speech bubble
             Destroy(speechBubble);
+            _oldLady.GetComponent<Animator>().enabled = false;
+            
+            yield return new WaitForSeconds(1f);
 
+
+            StartCoroutine(MoveBackToStart(visitor));
             //wait for the interaction delay
             yield return new WaitForSeconds(_interactionDelay);
         }
+
+        StartCoroutine(VisitorLoop());
     }
 
     IEnumerator MoveToPositon(GameObject visitor)
@@ -66,5 +82,18 @@ public class VisitorMovementController : MonoBehaviour
             timePassed += Time.deltaTime;
             yield return new WaitForEndOfFrame();
         }
+    }
+
+    IEnumerator MoveBackToStart(GameObject visitor)
+    {
+        float timePassed = 0;
+        Vector3 startPosition = visitor.transform.position;
+        while (timePassed < _interactionTime)
+        {
+            visitor.transform.position = Vector3.Lerp(_visitorDestination, _visitorSpawnPoint, timePassed / _interactionTime);
+            timePassed += Time.deltaTime;
+            yield return new WaitForEndOfFrame();
+        }
+        Destroy(visitor);
     }
 }
