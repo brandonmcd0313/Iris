005265a1bab4ef5587f90a1aba9e5ea9c616ddd0
shwoomgloom <svasilides2@gmail.com>
2023-10-19 07:23:09 -0400

    Menu programing

---
+++ Assets/Scripts/GameManagement/MenuManager.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.UI; //UI text and images
+using TMPro; //UI Text Mesh Pro
+using UnityEngine.SceneManagement; //loading levels
+using UnityEngine;
+
+public class MenuManager : MonoBehaviour
+{
+    public GameObject _loadingScreen;
+
+    public Button _startGameButton; //starts the button
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
