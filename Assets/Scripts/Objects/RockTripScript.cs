55df991ed39a081a01522ee015146825244f0d01
shwoomgloom <svasilides2@gmail.com>
2023-10-27 08:58:43 -0400

    Rock trip progress

--- Assets/Scripts/Objects/RockTripScript.cs
+++ Assets/Scripts/Objects/RockTripScript.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 
 public class RockTripScript : MonoBehaviour
 {
-    bool _playerCanTrip;
+    public bool _playerCanTrip;
    
     
     // Start is called before the first frame update
@@ -16,16 +16,31 @@ public class RockTripScript : MonoBehaviour
 
     void OnTriggerEnter2D(Collider2D _otherObject)
     {
+        print("Player hits rock.");
+
         if (_otherObject.tag == "Player" && _playerCanTrip == true)
         {
+            //remove the candy from inventory
+            print("inventory before removing candy from bag: " + PlayerPrefs.GetString("p_inventory"));
+            PlayerPrefsManager.RemoveAllOfObjectTypeFromInventory("candy");
+            print("inventory after removing candy from bag: " + PlayerPrefs.GetString("p_inventory"));
 
+            foreach (GameObject person in GameObject.FindGameObjectsWithTag("Crowd"))
+            {
+                person.GetComponent<MoveOnPlayerInteraction>().OnPlayerInteraction();
+            }
+
+        }
+        else if (_otherObject.tag == "Player" && _playerCanTrip == false)
+        {
+            print("Player's candy is safe :)   for now...");
         }
     }
 
 
     void CheckIfPlayerCanTrip()
     {
-        //player can interact if they have three troycoin
+        //player can trip if they have 3 or more candy
         string[] inventory = PlayerPrefsManager.GetItemsInInventory();
         int candyCount = 0;
         foreach (string item in inventory)
@@ -38,10 +53,12 @@ public class RockTripScript : MonoBehaviour
 
         if (candyCount >= 4)
         {
+            print("Player can trip");
             _playerCanTrip = true;
         }
         else
         {
+            //print("Player can't trip");
             _playerCanTrip = false;
         }
     }
