a325756eb81fe3cbb194757f326301bb6c49f97e
shwoomgloom <svasilides2@gmail.com>
2023-10-27 08:27:28 -0400

    Working on Rock trip

--- Assets/Scripts/Objects/RockTripScript.cs
+++ Assets/Scripts/Objects/RockTripScript.cs
@@ -4,15 +4,46 @@ using UnityEngine;
 
 public class RockTripScript : MonoBehaviour
 {
+    bool _playerCanTrip;
+   
+    
     // Start is called before the first frame update
     void Start()
     {
-        
+       InvokeRepeating("CheckIfPlayerCanTrip",0, 0.5f);
     }
 
-    // Update is called once per frame
-    void Update()
+
+    void OnTriggerEnter2D(Collider2D _otherObject)
     {
-        
+        if (_otherObject.tag == "Player" && _playerCanTrip == true)
+        {
+
+        }
     }
+
+
+    void CheckIfPlayerCanTrip()
+    {
+        //player can interact if they have three troycoin
+        string[] inventory = PlayerPrefsManager.GetItemsInInventory();
+        int candyCount = 0;
+        foreach (string item in inventory)
+        {
+            if (item == "candy")
+            {
+                candyCount++;
+            }
+        }
+
+        if (candyCount >= 4)
+        {
+            _playerCanTrip = true;
+        }
+        else
+        {
+            _playerCanTrip = false;
+        }
+    }
+
 }
