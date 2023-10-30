0f821639126d5d4b3d606651dbb0508478e7255d
shwoomgloom <svasilides2@gmail.com>
2023-10-30 08:11:20 -0400

    Credits and Reveal Poster

--- Assets/Scripts/GameManagement/CreditButton.cs
+++ Assets/Scripts/GameManagement/CreditButton.cs
@@ -1,9 +1,15 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement; //loading levels
+using UnityEngine.UI; //UI text and images
+using TMPro; //UI Text Mesh Pro
 
 public class CreditButton : MonoBehaviour
 {
+    AudioSource _audioSource;
+    public AudioClip _contSound;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -15,4 +21,12 @@ public class CreditButton : MonoBehaviour
     {
         
     }
+
+    public void ShowCredits()
+    {
+        //load credit scene
+       UnityEngine.SceneManagement.SceneManager.LoadScene("Credits");
+    }
+
+
 }
